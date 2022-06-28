461b2708519ac036cc327bc9052fddb9059f79c8
Gabriel Bruno Traldi <traldi13@hotmail.com>
2022-06-28 18:07:30 -0300

    fix: remove react router dom warning

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+
+import './config/i18n';
+
 import App from './App';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement!);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 );
